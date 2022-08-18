45633592759379d97fa52a94c085317059b20d8e
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-18 11:39:35 -0300

    feat: startapp and added installed_apps

--- django_app/settings.py
+++ django_app/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from config import settings
 from pathlib import Path
+
+from config import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "app",
 ]
 
 MIDDLEWARE = [
