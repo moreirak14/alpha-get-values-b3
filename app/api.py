1279b38949b747d36c15566925500f23891291d4
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-18 17:24:57 -0300

    feat: initial settings for sending the e-mail

--- app/api.py
+++ app/api.py
@@ -1,18 +1,21 @@
+import email.message
+import smtplib
+
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from config import settings
-import requests
 
 
 class GetValuesApi(APIView):
-
     def get(self, request):
         """
-        Return the price of an asset
+        Returns the price of an asset and sends a value alert via e-mail
         """
 
-        asset_price = "32.0"
+        asset_price = "33.0"
         symbol = "PETR4.SA"
         url = f"{settings.ALPHA_VANTAGE}/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={settings.API_KEY}"
         response = requests.get(url=url)
@@ -20,18 +23,41 @@ class GetValuesApi(APIView):
 
         for value in data.values():
             price = value.get("05. price")
-            # print(f"Preço atual: {price}")
-            # print(f"Preço que desejo comprar: {asset_price}")
 
-            if price <= asset_price:
-                return Response(data={
-                    "active": symbol,
-                    "price": price,
-                    "message": "buy"
-                }, status=status.HTTP_200_OK)
-            else:
-                return Response(data={
-                    "active": symbol,
-                    "price": price,
-                    "message": "not buy"
-                }, status=status.HTTP_400_BAD_REQUEST)
+            if price > asset_price:
+                return Response(
+                    data={"active": symbol, "price": price, "message": "not buy"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            self._send_email(symbol=symbol, price=price)
+            return Response(
+                data={"active": symbol, "price": price, "message": "buy"},
+                status=status.HTTP_200_OK,
+            )
+
+    def _send_email(self, symbol: str, price: str):
+        body = f"""
+        <p>Olá, Investidor!</p>
+        <p>A cotação do seu ativo chegou ao valor estabelecido! E ai, bora comprar?</p>
+        <p>Ativo: {symbol}, Valor: R$ {price}</p>
+        <p>E-mail enviado automaticamente.</p>
+        """
+
+        """
+        Initial settings for sending the e-mail
+        """
+        msg = email.message.Message()
+        msg["Subject"] = "Cotação Diária de Ativos"
+        msg["From"] = settings.EMAIL
+        msg["To"] = settings.EMAIL
+        msg.add_header("Content-Type", "text/html")
+        msg.set_payload(payload=body)
+
+        """
+        Login Credentials for sending the e-mail
+        """
+        send_email = smtplib.SMTP("smtp.gmail.com: 587")
+        send_email.starttls()
+        send_email.login(msg["From"], settings.PASSWORD)
+        send_email.sendmail(msg["From"], [msg["To"]], msg.as_string().encode("utf-8"))
