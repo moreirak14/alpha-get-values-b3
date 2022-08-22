a054b68dae6fe75304788dd68cc3d72921efeb01
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-22 15:15:50 -0300

    feat: send email when buy or sell

--- app/api.py
+++ app/api.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from config import settings
-from src.services.email import send_email
+from src.services.email import send_email_buy, send_email_sell
 
 
 class GetValuesApi(APIView):
@@ -14,24 +14,25 @@ class GetValuesApi(APIView):
         Returns the price of an asset and sends a value alert via e-mail
         """
 
-        data = request.data
-        asset_price = data.get("asset_price")
-        symbol = data.get("symbol")
+        data_input = request.data
+        asset_price = data_input.get("asset_price")
+        symbol = data_input.get("symbol")
         url = f"{settings.ALPHA_VANTAGE}/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={settings.API_KEY}"
         response = requests.get(url=url)
-        data = response.json()
+        data_output = response.json()
 
-        for value in data.values():
+        for value in data_output.values():
             price = value.get("05. price")
 
-            if price > asset_price:
+            if price >= asset_price:
+                send_email_sell(symbol=symbol, price=price)
                 return Response(
-                    data={"active": symbol, "price": price, "message": "not buy"},
-                    status=status.HTTP_400_BAD_REQUEST,
+                    data={"active": symbol, "price": price, "message": "sell"},
+                    status=status.HTTP_200_OK,
                 )
-
-            send_email(symbol=symbol, price=price)
-            return Response(
-                data={"active": symbol, "price": price, "message": "buy"},
-                status=status.HTTP_200_OK,
-            )
+            else:
+                send_email_buy(symbol=symbol, price=price)
+                return Response(
+                    data={"active": symbol, "price": price, "message": "buy"},
+                    status=status.HTTP_200_OK,
+                )
