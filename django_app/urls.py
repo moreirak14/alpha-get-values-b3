51b63a60cbaf16dccffbdd93da220f703cc09acb
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-22 22:12:34 -0300

    feat: settings DRF simplejwt

--- django_app/urls.py
+++ django_app/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
+    path("api/token/verify/", TokenVerifyView.as_view()),
     path("api/app/", include("app.urls")),
     path("admin/", admin.site.urls),
 ]
