72ddb7707b2418924b7cef43cc2b18e7c8e7bdaf
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-23 21:03:50 -0300

    fix

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .api import RegisterView, RetrieveUserView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view()),
-    path("me/", RetrieveUserView.as_view()),
+    path("register", RegisterView.as_view()),
+    path("me", RetrieveUserView.as_view()),
 ]
