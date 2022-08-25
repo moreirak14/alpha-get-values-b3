ecaac261383f2c1c86fc926513a0f091d68eaa07
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-24 22:42:52 -0300

    feat: created action model and migrate new model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from users.models import UserAccount
+
+
+class Action(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=10, null=False)
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.symbol
+
+    class Meta:
+        db_table = "Action"
+        verbose_name = "Action"
+        verbose_name_plural = "Actions"
