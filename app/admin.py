15f0a60570ecec0baa672e777488ee9c6cdeabc9
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-24 22:44:11 -0300

    feat: register model in admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Action
+
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    pass
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserAccount
+
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    pass
